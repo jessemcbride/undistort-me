96499514cfa9b20e73cae1102e3b4fd811692ee2
Jesse McBride <mcbjesse@gmail.com>
2017-10-21 12:07:54 -0400

    Add nodemon, start prototyping signup

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'undistort.me' });
 });
 
+router.get('/signup', function(req, res, next) {
+	res.render('accounts/signup', {title: 'undistort.me' });
+});
+
 module.exports = router;
